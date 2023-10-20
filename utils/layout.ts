79c16444ce457958866ea059346689c98debe555
Khang Nguyễn <thinhkhang97@gmail.com>
2023-10-20 10:02:51 +0700

    fix(lib): :bug: check intersection condition

--- utils/layout.ts
+++ utils/layout.ts
@@ -12,14 +12,36 @@ export const checkInViewPort = (
   percentWidth = 1,
   percentHeight = 1
 ) => {
-  const intersectWidth = Math.abs(
-    Math.min(childView.x + childView.width, parentView.x + parentView.width) -
-      Math.max(childView.x, parentView.x)
-  );
-  const intersectHeight = Math.abs(
-    Math.min(childView.y + childView.height, parentView.y + parentView.height) -
-      Math.max(childView.y, parentView.y)
-  );
+  let intersectWidth = 0,
+    intersectHeight = 0;
+  if (
+    Math.min(childView.x, parentView.x) +
+      Math.max(
+        childView.x + childView.width,
+        parentView.x + parentView.width
+      ) <=
+    childView.width + parentView.width
+  ) {
+    intersectWidth = Math.abs(
+      Math.min(childView.x + childView.width, parentView.x + parentView.width) -
+        Math.max(childView.x, parentView.x)
+    );
+  }
+  if (
+    Math.min(childView.y, parentView.y) +
+      Math.max(
+        childView.y + childView.height,
+        parentView.y + parentView.height
+      ) <=
+    childView.height + parentView.height
+  ) {
+    intersectHeight = Math.abs(
+      Math.min(
+        childView.y + childView.height,
+        parentView.y + parentView.height
+      ) - Math.max(childView.y, parentView.y)
+    );
+  }
   return (
     intersectWidth / childView.width >= percentWidth &&
     intersectHeight / childView.height >= percentHeight
